1768489affbf202b28fc733eca263cf675d06f3b
Doyle Mills <doyle.j.mills@nasa.gov>
2020-06-12 08:03:23 -0500

    Update psp to generic bsp.

--- fsw/sp0-vxworks6.9/inc/psp_version.h
+++ fsw/sp0-vxworks6.9/inc/psp_version.h
@@ -19,10 +19,12 @@
 #ifndef _psp_version_
 #define _psp_version_
 
+
 #define CFE_PSP_IMPL_MAJOR_VERSION          1
-#define CFE_PSP_IMPL_MINOR_VERSION          3
-#define CFE_PSP_IMPL_REVISION               90
+#define CFE_PSP_IMPL_MINOR_VERSION          4
+#define CFE_PSP_IMPL_REVISION               13
 #define CFE_PSP_IMPL_MISSION_REV            0
+
 
 /*
 ** Macro Definitions
--- fsw/sp0-vxworks6.9/make/build_options.cmake
+++ fsw/sp0-vxworks6.9/make/build_options.cmake
@@ -22,3 +22,5 @@ add_definitions("-mcpu=8548 -te500v2 -mstrict-align -fno-builtin -fvolatile -mha
 include_directories($ENV{WIND_BASE}/target/h/wrn/coreip)
 include_directories($ENV{WIND_BASE}/target/3rdparty/aitech/sp0)
 include_directories($ENV{WIND_BASE}/target/config/sp0)
+
+set(CFE_PSP_EXPECTED_OSAL_BSPTYPE "generic-vxworks")
