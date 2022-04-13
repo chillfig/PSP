01c846cb0c5e560caf0d6df24f5f7e55030eec8d
Ngo, Tam M 250827735 <tam.m.ngo@nasa.gov>
2022-04-13 18:24:19 +0000

    GWCFS-1308 PSP - Update gen_tables.sh script

--- docs/gen_tables.sh
+++ docs/gen_tables.sh
@@ -23,22 +23,25 @@ function gen_tables_from_doxygen_comments()
     mv cfe_PSP_memory.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_memory.pdf
 
     python3 gen_api_tbls.py psp $SW_DIR/fsw/sp0-vxworks6.9/inc/cfe_psp_exception.h "SP0 Exceptions" && \
-    mv PSP_exceptions.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_exceptions.pdf
+    mv cfe_PSP_exception.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_exceptions.pdf
 
     python3 gen_api_tbls.py psp $SW_DIR/fsw/sp0-vxworks6.9/inc/cfe_psp_flash.h "FLASH" && \
-    mv PSP_flash.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_flash.pdf
+    mv cfe_PSP_flash.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_flash.pdf
 
     python3 gen_api_tbls.py psp $SW_DIR/fsw/sp0-vxworks6.9/inc/cfe_psp_memscrub.h "Memory Scrub" && \
-    mv PSP_mem_scrub.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_memoryscrub.pdf
+    mv cfe_PSP_memscrub.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_memoryscrub.pdf
 
     python3 gen_api_tbls.py psp $SW_DIR/fsw/sp0-vxworks6.9/inc/cfe_psp_sp0info.h "SP0 Data" && \
-    mv PSP_sp0_info.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_sp0.pdf
+    mv cfe_PSP_sp0info.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_sp0.pdf
 
     python3 gen_api_tbls.py psp $SW_DIR/fsw/sp0-vxworks6.9/inc/cfe_psp_start.h "Startup" && \
-    mv PSP_start.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_startup.pdf
+    mv cfe_PSP_start.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_startup.pdf
 
-    python3 gen_api_tbls.py psp $SW_DIR/fsw/sp0-vxworks6.9/inc/cfe_psp_timesync.h "Time Sync" && \
-    mv PSP_time_sync.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_timesync.pdf
+    python3 gen_api_tbls.py psp $SW_DIR/fsw/sp0-vxworks6.9/inc/cfe_psp_support.h "Startup" && \
+    mv cfe_PSP_support.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_support.pdf
+
+    python3 gen_api_tbls.py psp $SW_DIR/fsw/shared/inc/cfe_psp_timesync.h "Time Sync" && \
+    mv cfe_PSP_timesync.pdf $SW_DIR/docs/PSP_sp0-vxworks6.9_timesync.pdf
 
     echo -e "\n2. Remove un-needed files\n"
     rm -f *.docx
